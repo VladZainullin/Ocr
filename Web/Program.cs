3a38e03ba5bd23a0a2bfc399d310047227c07292
Vladislav Zainullin <mittoface02@mail.ru>
2025-10-30 21:56:09 +0300

    Добавляет реализацию распознования текста на 1 странице документа

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,4 +1,5 @@
 using Tesseract;
+using UglyToad.PdfPig;
 
 namespace Web;
 
@@ -8,9 +9,50 @@ file sealed class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        using var engine = new TesseractEngine("./tesseract", "rus", EngineMode.Default);
+        builder.Services.AddSingleton(static sp =>
+        {
+            var applicationLifetime = sp.GetRequiredService<IHostApplicationLifetime>();
+
+            var engine = new TesseractEngine("./tesseract", "rus");
+            applicationLifetime.ApplicationStopping.Register(() => engine.Dispose());
+
+            return engine;
+        });
 
         await using var app = builder.Build();
+
+        app.MapPost("/documents", static async context =>
+        {
+            var engine = context.RequestServices.GetRequiredService<TesseractEngine>();
+            await using var file = context.Request.Form.Files[0].OpenReadStream();
+            using var document = PdfDocument.Open(file);
+            var pages = document.GetPages();
+
+            foreach (var page in pages)
+            {
+                var images = page.GetImages();
+
+                foreach (var image in images)
+                {
+                    if (image.TryGetPng(out var pngBytes))
+                    {
+                        using var imageDocument = Pix.LoadFromMemory(pngBytes);
+                        using var imagePage = engine.Process(imageDocument);
+                        var text = imagePage.GetText();
+                        await context.Response.WriteAsJsonAsync(text);
+                        return;
+                    }
+                    else
+                    {
+                        using var imageDocument = Pix.LoadFromMemory(image.RawBytes.ToArray());
+                        using var imagePage = engine.Process(imageDocument);
+                        var text = imagePage.GetText();
+                        await context.Response.WriteAsJsonAsync(text);
+                        return;
+                    }
+                }
+            }
+        });
 
         await app.RunAsync();
     }
