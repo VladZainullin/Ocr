db543fad3f7cb83a114f75852d39cf7ceb38fbd5
Vladislav Zainullin <mittoface02@mail.ru>
2025-10-30 19:07:09 +0300

    Подключает Tesseract

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,13 +1,17 @@
+using Tesseract;
+
 namespace Web;
 
 file sealed class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        
-        var app = builder.Build();
 
-        app.Run();
+        using var engine = new TesseractEngine("./tessdata", "rus", EngineMode.Default);
+
+        await using var app = builder.Build();
+
+        await app.RunAsync();
     }
 }
