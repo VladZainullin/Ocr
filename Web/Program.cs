033d134f11432415b1930c4c58b3096fd0c77f0d
VladZainullin <mittoface02@mail.ru>
2025-11-07 16:21:13 +0300

    Добавляет версию с временными файлами

--- Web/Program.cs
+++ Web/Program.cs
@@ -48,6 +48,108 @@ file sealed class Program
 
             var pdfPages = pdfDocument.GetPages();
 
+            var pageResponses = new PageResponse[pdfDocument.NumberOfPages];
+            foreach (var pdfPage in pdfPages)
+            {
+                var pdfImages = pdfPage.GetImages();
+                var imageResponses = new List<string>();
+                foreach (var pdfImage in pdfImages)
+                {
+                    var imageBytes = GetImageBytes(pdfImage);
+                    if (imageBytes.Length == 0) continue;
+
+                    var tempFilePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                    tempFilePath = Path.ChangeExtension(tempFilePath, ".tmp");
+
+                    await using var fileStream = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write);
+                    try
+                    {
+                        await fileStream.WriteAsync(imageBytes);
+                        imageResponses.Add(tempFilePath);
+                    }
+                    catch
+                    {
+                        fileStream.Close();
+                        File.Delete(tempFilePath);
+                        throw;
+                    }
+                    finally
+                    {
+                        fileStream.Close();
+                    }
+                }
+
+                pageResponses[pdfPage.Number - 1] = new PageResponse
+                {
+                    Number = pdfPage.Number,
+                    Text = pdfPage.Text,
+                    Images = imageResponses
+                };
+            }
+
+            await Parallel.ForEachAsync(pageResponses, new ParallelOptions
+            {
+                MaxDegreeOfParallelism = Environment.ProcessorCount
+            }, async (page, _) =>
+            {
+                var imageResponses = new List<string>();
+                foreach (var image in page.Images)
+                {
+                    await using var fileStream = new FileStream(image, FileMode.Open, FileAccess.Read);
+                    try
+                    {
+                        var preparateImageBytes = PreparateImage(fileStream);
+                        var engine = tesseractEngineObjectPool.Get();
+                        if (preparateImageBytes.Length == 0) continue;
+                        try
+                        {
+                            using var imageDocument = Pix.LoadFromMemory(preparateImageBytes);
+                            using var imagePage = engine.Process(imageDocument);
+                            var text = imagePage.GetText();
+                            imageResponses.Add(text);
+                        }
+                        finally
+                        {
+                            tesseractEngineObjectPool.Return(engine);
+                        }
+                    }
+                    finally
+                    {
+                        fileStream.Close();
+                        File.Delete(image);
+                    }
+                }
+
+                page.Images = imageResponses;
+            });
+
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                Pages = pageResponses,
+            });
+        });
+
+        app.MapPost("api/v1/documents", static async context =>
+        {
+            if (context.Request.Form.Files.Count < 1
+                || context.Request.Form.Files[0].ContentType != MediaTypeNames.Application.Pdf
+                || context.Request.Form.Files[0].Length == 0)
+            {
+                await context.Response.WriteAsJsonAsync(new Response
+                {
+                    Pages = [],
+                });
+                return;
+            }
+
+            await using var stream = context.Request.Form.Files[0].OpenReadStream();
+
+            using var pdfDocument = PdfDocument.Open(stream);
+
+            var tesseractEngineObjectPool = context.RequestServices.GetRequiredService<ObjectPool<TesseractEngine>>();
+
+            var pdfPages = pdfDocument.GetPages();
+
             var pageResponses = ArrayPool<PageResponse>.Shared.Rent(pdfDocument.NumberOfPages);
             try
             {
@@ -93,94 +195,6 @@ file sealed class Program
                 ArrayPool<PageResponse>.Shared.Return(pageResponses);
             }
         });
-        
-        app.MapPost("api/v1/documents", static async context =>
-        {
-            if (context.Request.Form.Files.Count < 1
-                || context.Request.Form.Files[0].ContentType != MediaTypeNames.Application.Pdf
-                || context.Request.Form.Files[0].Length == 0)
-            {
-                await context.Response.WriteAsJsonAsync(new Response
-                {
-                    Pages = [],
-                });
-                return;
-            }
-
-            await using var stream = context.Request.Form.Files[0].OpenReadStream();
-
-            using var pdfDocument = PdfDocument.Open(stream);
-
-            var tesseractEngineObjectPool = context.RequestServices.GetRequiredService<ObjectPool<TesseractEngine>>();
-
-            var pageResponses = new LinkedList<PageResponse>();
-
-            var pdfPages = pdfDocument.GetPages();
-
-            foreach (var pdfPagesInChunk in pdfPages.Chunk(Environment.ProcessorCount))
-            {
-                var chunk = new List<Buffer>();
-
-                foreach (var pdfPage in pdfPagesInChunk)
-                {
-                    var pdfImages = pdfPage.GetImages();
-                    var memories = new List<Memory<byte>>();
-                    foreach (var pdfImage in pdfImages)
-                    {
-                        var imageBytes = GetImageBytes(pdfImage);
-                        if (imageBytes.Length == 0) continue;
-                        memories.Add(imageBytes);
-                    }
-
-                    chunk.Add(new Buffer
-                    {
-                        Number = pdfPage.Number,
-                        Text = pdfPage.Text,
-                        Images = memories
-                    });
-                }
-
-                Parallel.ForEach(
-                    chunk,
-                    new ParallelOptions
-                    {
-                        MaxDegreeOfParallelism = Environment.ProcessorCount,
-                    },
-                    buffer =>
-                    {
-                        var imageResponses = new LinkedList<string>();
-                        foreach (var imageMemory in buffer.Images)
-                        {
-                            var preparateImageBytes = PreparateImage(imageMemory.Span);
-                            if (preparateImageBytes.Length == 0) return;
-                            var engine = tesseractEngineObjectPool.Get();
-                            try
-                            {
-                                using var imageDocument = Pix.LoadFromMemory(preparateImageBytes);
-                                using var imagePage = engine.Process(imageDocument);
-                                var text = imagePage.GetText();
-                                imageResponses.AddLast(text);
-                            }
-                            finally
-                            {
-                                tesseractEngineObjectPool.Return(engine);
-                            }
-                        }
-
-                        pageResponses.AddLast(new PageResponse
-                        {
-                            Number = buffer.Number,
-                            Text = buffer.Text,
-                            Images = imageResponses
-                        });
-                    });
-            }
-
-            await context.Response.WriteAsJsonAsync(new Response
-            {
-                Pages = pageResponses,
-            });
-        });
 
         await app.RunAsync();
     }
@@ -214,6 +228,21 @@ file sealed class Program
             return [];
         }
     }
+
+    private static byte[] PreparateImage(Stream stream)
+    {
+        try
+        {
+            using var image = new MagickImage(stream);
+            image.Grayscale();
+            image.Strip();
+            return image.ToByteArray();
+        }
+        catch (MagickMissingDelegateErrorException)
+        {
+            return [];
+        }
+    }
 }
 
 public sealed class Buffer
@@ -230,11 +259,20 @@ public sealed class Response
     public required IReadOnlyCollection<PageResponse> Pages { get; init; }
 }
 
+public sealed class PageBuffer
+{
+    public required int Number { get; set; }
+
+    public required string Text { get; init; }
+
+    public required string Path { get; init; }
+}
+
 public sealed class PageResponse
 {
     public required int Number { get; init; }
 
     public required string Text { get; init; }
 
-    public required IEnumerable<string> Images { get; init; }
+    public required IReadOnlyCollection<string> Images { get; set; }
 }
