7257c7045452cd1af40434ca835f99d5326758d8
Vladislav Zainullin <mittoface02@mail.ru>
2025-11-09 14:58:11 +0300

    Добавляет возврат степени уверенности в распозновании

--- Web/Program.cs
+++ Web/Program.cs
@@ -95,6 +95,7 @@ file sealed class Program
                         if (text == string.Empty) return;
                         searchTextBuffers[imageTextBuffer.Number - 1].Images.Add(new ImageResponse
                         {
+                            Confidence = imagePage.GetMeanConfidence(),
                             Blocks = blocks,
                         });
                     }
@@ -255,6 +256,7 @@ file sealed class Program
 
 public sealed class ImageResponse
 {
+    public required float Confidence { get; set; }
     public required List<BlockResponse> Blocks { get; set; }
 }
 
