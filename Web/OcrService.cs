4e4d48d17440cc67b81e5e3f10182b8e3f6a096f
Vladislav Zainullin <mittoface02@mail.ru>
2025-11-15 13:50:43 +0300

    Добавляет OcrService

---
+++ Web/OcrService.cs
@@ -0,0 +1,98 @@
+using Microsoft.Extensions.ObjectPool;
+using Tesseract;
+
+namespace Web;
+
+internal sealed class OcrService(ObjectPool<TesseractEngine> pool)
+{
+    public IEnumerable<BlockResponse> Process(byte[] bytes)
+    {
+        var tesseractEngine = pool.Get();
+        try
+        {
+            var pix = Pix.LoadFromMemory(bytes);
+            var page = tesseractEngine.Process(pix);
+            using var iterator = page.GetIterator();
+            
+            BlockResponse? currentBlock = null;
+            LineResponse? currentLine = null;
+            
+            do
+            {
+                if (iterator.IsAtBeginningOf(PageIteratorLevel.Block))
+                {
+                    currentBlock = new BlockResponse();
+                }
+
+                if (iterator.IsAtBeginningOf(PageIteratorLevel.TextLine))
+                {
+                    currentLine = new LineResponse();
+                }
+
+                if (iterator.IsAtBeginningOf(PageIteratorLevel.Word))
+                {
+                    var word = iterator.GetText(PageIteratorLevel.Word);
+                    if (!string.IsNullOrWhiteSpace(word))
+                    {
+                        currentLine?.Words.Add(word);
+                    }
+                }
+
+                if (iterator.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word) && currentLine?.Words.Count > 0)
+                {
+                    currentBlock?.Lines.Add(currentLine);
+                }
+
+                if (iterator.IsAtFinalOf(PageIteratorLevel.Block, PageIteratorLevel.Word) && currentBlock?.Lines.Count > 0)
+                {
+                    yield return currentBlock;
+                }
+            } while (iterator.Next(PageIteratorLevel.Word));
+        }
+        finally
+        {
+            pool.Return(tesseractEngine);
+        }
+    }
+    
+    private IEnumerable<BlockResponse> ExtractLayoutFromPage(Page page)
+    {
+        using var iter = page.GetIterator();
+        iter.Begin();
+
+        BlockResponse? currentBlock = null;
+        LineResponse? currentLine = null;
+
+        do
+        {
+            if (iter.IsAtBeginningOf(PageIteratorLevel.Block))
+            {
+                currentBlock = new BlockResponse();
+            }
+
+            if (iter.IsAtBeginningOf(PageIteratorLevel.TextLine))
+            {
+                currentLine = new LineResponse();
+            }
+
+            if (iter.IsAtBeginningOf(PageIteratorLevel.Word))
+            {
+                var word = iter.GetText(PageIteratorLevel.Word);
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    currentLine?.Words.Add(word);
+                }
+            }
+
+            if (iter.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word) && currentLine?.Words.Count > 0)
+            {
+                currentBlock?.Lines.Add(currentLine);
+            }
+
+            if (iter.IsAtFinalOf(PageIteratorLevel.Block, PageIteratorLevel.Word) && currentBlock?.Lines.Count > 0)
+            {
+                yield return currentBlock;
+            }
+        } while (iter.Next(PageIteratorLevel.Word));
+    }
+}
