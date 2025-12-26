a9d504287011b1bc5acf980b27f3e1f87b092f25
Vladislav Zainullin <mittoface02@mail.ru>
2025-12-26 20:41:17 +0300

    Убирает поддержку нового синтаксиса

--- OcrService/DependencyInjection.cs
+++ OcrService/DependencyInjection.cs
@@ -9,33 +9,30 @@ namespace OcrService;
 
 public static class DependencyInjection
 {
-    extension(IHostApplicationBuilder builder)
+    public static IHostApplicationBuilder AddOcr(this IHostApplicationBuilder builder)
     {
-        public IHostApplicationBuilder AddOcr()
+        builder.Services
+            .AddOptions<TesseractOptions>()
+            .BindConfiguration("Tesseract")
+            .Validate(
+                static tesseractOptions => !string.IsNullOrWhiteSpace(tesseractOptions.Language), 
+                """Tesseract Language is required. Add "Tesseract__Language" to environment variables""")
+            .Validate(
+                static tesseractOptions => !string.IsNullOrWhiteSpace(tesseractOptions.Path), 
+                """Tesseract Path is required. Add "Tesseract__Path" to environment variables""")
+            .ValidateOnStart();
+        
+        builder.Services.TryAddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+        builder.Services.TryAddSingleton<TesseractEnginePooledObjectPolicy>();
+        builder.Services.TryAddSingleton<ObjectPool<TesseractEngine>>(static serviceProvider =>
         {
-            builder.Services
-                .AddOptions<TesseractOptions>()
-                .BindConfiguration("Tesseract")
-                .Validate(
-                    static tesseractOptions => !string.IsNullOrWhiteSpace(tesseractOptions.Language), 
-                    """Tesseract Language is required. Add "Tesseract__Language" to environment variables""")
-                .Validate(
-                    static tesseractOptions => !string.IsNullOrWhiteSpace(tesseractOptions.Path), 
-                    """Tesseract Path is required. Add "Tesseract__Path" to environment variables""")
-                .ValidateOnStart();
+            var provider = serviceProvider.GetRequiredService<ObjectPoolProvider>();
+            var policy = serviceProvider.GetRequiredService<TesseractEnginePooledObjectPolicy>();
+            return provider.Create(policy);
+        });
         
-            builder.Services.TryAddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
-            builder.Services.TryAddSingleton<TesseractEnginePooledObjectPolicy>();
-            builder.Services.TryAddSingleton<ObjectPool<TesseractEngine>>(static serviceProvider =>
-            {
-                var provider = serviceProvider.GetRequiredService<ObjectPoolProvider>();
-                var policy = serviceProvider.GetRequiredService<TesseractEnginePooledObjectPolicy>();
-                return provider.Create(policy);
-            });
-        
-            builder.Services.TryAddSingleton<IOcrService, OcrService>();
+        builder.Services.TryAddSingleton<IOcrService, OcrService>();
             
-            return builder;
-        }
+        return builder;
     }
 }
