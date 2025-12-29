feae65070c85c399b55aba95ff442c41a299f4f8
VladZainullin <mittoface02@mail.ru>
2025-12-29 15:04:06 +0300

    Добавляет endpoint для распознования изображений

---
+++ Web/Modules/ImagesModule.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Mime;
+using Carter;
+using Domain;
+using ImageService.Contracts;
+using OcrService.Contracts;
+
+namespace Web.Modules;
+
+public sealed class ImagesModule : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/v3/images", static async context =>
+        {
+            if (context.Request.Form.Files.Count < 1
+                || context.Request.Form.Files[0].ContentType != MediaTypeNames.Image.Jpeg
+                || context.Request.Form.Files[0].Length == 0)
+            {
+                await context.Response.WriteAsJsonAsync(new ImageModel
+                {
+                    Blocks = []
+                });
+                return;
+            }
+
+            var imageService = context.RequestServices.GetRequiredService<IImageService>();
+            var ocrService = context.RequestServices.GetRequiredService<IOcrService>();
+
+            await using var stream = context.Request.Form.Files[0].OpenReadStream();
+            var bytes = imageService.Recognition(stream);
+            var blocks = ocrService.Process(bytes);
+
+            await context.Response.WriteAsJsonAsync(new ImageModel
+            {
+                Blocks = blocks
+            });
+        });
+    }
+}
