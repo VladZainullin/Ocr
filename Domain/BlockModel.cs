f9c7902d50908473bf9c106a1bfbfe0ebe88f4a7
VladZainullin <mittoface02@mail.ru>
2026-03-20 07:53:34 +0300

    Упрощает процесс считывания машиночитаемого текста в Pdf

--- Domain/BlockModel.cs
+++ Domain/BlockModel.cs
@@ -4,5 +4,5 @@ public sealed class BlockModel
 {
     public string Text { get; set; } = null!;
     
-    public List<LineModel> Lines { get; } = [];
+    public List<LineModel> Lines { get; init; } = [];
 }
--- Domain/LineModel.cs
+++ Domain/LineModel.cs
@@ -3,5 +3,5 @@ namespace Domain;
 public sealed class LineModel
 {
     public string Text { get; set; } = null!;
-    public List<string> Words { get; } = [];
+    public List<string> Words { get; init; } = [];
 }
