3070331f645e626ce87129affe95c075f1d6c84b
VladZainullin <mittoface02@mail.ru>
2026-06-26 12:07:44 +0300

    Добавляет метод в TesseractLibrary.cs

--- Vlad.Tesseract/TesseractLibrary.cs
+++ Vlad.Tesseract/TesseractLibrary.cs
@@ -6,6 +6,7 @@ public unsafe class TesseractLibrary : IDisposable
 {
     private readonly nint _libraryHandle;
     private readonly delegate* unmanaged[Cdecl]<nint> _tessVersion;
+    private readonly delegate* unmanaged[Cdecl]<nint, nint> _tessBaseApiVersion;
     private bool _disposed;
 
     public TesseractLibrary(string dllPath)
@@ -17,14 +18,28 @@ public unsafe class TesseractLibrary : IDisposable
             throw new DllNotFoundException($"Failed to load library: {dllPath}");
 
         var versionPtr = NativeLibrary.GetExport(_libraryHandle, "TessVersion");
-
         if (versionPtr == nint.Zero)
             throw new EntryPointNotFoundException("TessVersion not found in the library");
-        
         _tessVersion = (delegate* unmanaged[Cdecl]<nint>)versionPtr;
+
+        var baseApiVersionPtr = NativeLibrary.GetExport(_libraryHandle, "TessBaseAPIVersion");
+        if (baseApiVersionPtr == nint.Zero)
+            throw new EntryPointNotFoundException("TessBaseAPIVersion not found in the library");
+        _tessBaseApiVersion = (delegate* unmanaged[Cdecl]<nint, nint>)baseApiVersionPtr;
     }
 
+    /// <summary>
+    /// Returns the version string of the Tesseract library.
+    /// </summary>
+    /// <returns>Pointer to a null-terminated UTF-8 string containing the version. The caller must NOT free this pointer.</returns>
     public nint GetVersion() => _tessVersion();
+
+    /// <summary>
+    /// Returns the version string of the Tesseract library associated with the given BaseAPI handle.
+    /// </summary>
+    /// <param name="handle">Pointer to the TessBaseAPI instance.</param>
+    /// <returns>Pointer to a null-terminated UTF-8 string containing the version.</returns>
+    public nint TessBaseApiVersion(nint handle) => _tessBaseApiVersion(handle);
 
     public void Dispose()
     {
