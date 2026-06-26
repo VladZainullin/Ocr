f64b42d8449578ef140a96af2cb9746cd7377218
VladZainullin <mittoface02@mail.ru>
2026-06-26 08:24:16 +0300

    Дополняет класс методами для взаимодейтсвия с ResultIterator

--- Vlad.Tesseract/Native.cs
+++ Vlad.Tesseract/Native.cs
@@ -6,7 +6,7 @@ namespace Vlad.Tesseract;
 internal static partial class Native
 {
     private const string DllName = @"C:\Users\user\RiderProjects\Ocr\Web\bin\Debug\net10.0\x64\tesseract50.dll";
-    
+
     [LibraryImport(DllName, EntryPoint = "TessPageIteratorBegin")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     public static partial void TessPageIteratorBegin(IntPtr iterator);
@@ -33,7 +33,7 @@ internal static partial class Native
     [LibraryImport(DllName, EntryPoint = "TessPageIteratorBaseline")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static partial bool TessPageIteratorBaseline(IntPtr iterator, PageIteratorLevel level, out int x1, 
+    public static partial bool TessPageIteratorBaseline(IntPtr iterator, PageIteratorLevel level, out int x1,
         out int y1, out int x2, out int y2);
 
     [LibraryImport(DllName, EntryPoint = "TessPageIteratorOrientation")]
@@ -42,7 +42,7 @@ internal static partial class Native
         IntPtr iterator,
         out OrientationPage orientation,
         out WritingDirection writingDirection,
-        out TextlineOrder textlineOrder, 
+        out TextlineOrder textlineOrder,
         out float deskewAngle);
 
     [LibraryImport(DllName, EntryPoint = "TessPageIteratorParagraphInfo")]
@@ -63,15 +63,15 @@ internal static partial class Native
     [return: MarshalAs(UnmanagedType.Bool)]
     public static partial bool TessPageIteratorGetWordFontAttributes(
         IntPtr iterator,
-        [MarshalAs(UnmanagedType.Bool)] out bool isBold, 
-        [MarshalAs(UnmanagedType.Bool)] out bool isItalic, 
-        [MarshalAs(UnmanagedType.Bool)] out bool isUnderlined, 
-        [MarshalAs(UnmanagedType.Bool)] out bool isMonospace, 
+        [MarshalAs(UnmanagedType.Bool)] out bool isBold,
+        [MarshalAs(UnmanagedType.Bool)] out bool isItalic,
+        [MarshalAs(UnmanagedType.Bool)] out bool isUnderlined,
+        [MarshalAs(UnmanagedType.Bool)] out bool isMonospace,
         [MarshalAs(UnmanagedType.Bool)] out bool isSerif,
-        [MarshalAs(UnmanagedType.Bool)] out bool isSmallCaps, 
+        [MarshalAs(UnmanagedType.Bool)] out bool isSmallCaps,
         out int pointSize,
         out int fontId);
-    
+
     [LibraryImport(DllName, EntryPoint = "TessBaseAPICreate")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     public static partial IntPtr TessBaseApiCreate();
@@ -126,4 +126,109 @@ internal static partial class Native
     [LibraryImport(DllName, EntryPoint = "TessPageIteratorDelete")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     public static partial void TessPageIteratorDelete(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorCopy")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial IntPtr TessResultIteratorCopy(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorDelete")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial void TessResultIteratorDelete(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorNext")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool TessResultIteratorNext(IntPtr iterator, PageIteratorLevel level);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorIsAtFinalElement")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool TessResultIteratorIsAtFinalElement(IntPtr iterator, PageIteratorLevel level,
+        PageIteratorLevel element);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorGetPageIterator")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial IntPtr TessResultIteratorGetPageIterator(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorGetPageIteratorConst")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial IntPtr TessResultIteratorGetPageIteratorConst(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorGetImage")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial IntPtr TessResultIteratorGetImage(IntPtr iterator, PageIteratorLevel level, int padding,
+        IntPtr originalImage, out int left, out int top);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorBoundingBox")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool TessResultIteratorBoundingBox(IntPtr iterator, PageIteratorLevel level,
+        out int left, out int top, out int right, out int bottom);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorBaseline")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool TessResultIteratorBaseline(IntPtr iterator, PageIteratorLevel level,
+        out int x1, out int y1, out int x2, out int y2);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorBlockType")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial PolyBlockType TessResultIteratorBlockType(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorGetBinaryImage")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial IntPtr TessResultIteratorGetBinaryImage(IntPtr iterator, PageIteratorLevel level);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorGetConfidence")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial float TessResultIteratorGetConfidence(IntPtr iterator, PageIteratorLevel level);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorWordRecognitionLanguage")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial IntPtr TessResultIteratorWordRecognitionLanguage(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorWordFontAttributes")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool TessResultIteratorWordFontAttributes(
+        IntPtr iterator,
+        [MarshalAs(UnmanagedType.Bool)] out bool isBold,
+        [MarshalAs(UnmanagedType.Bool)] out bool isItalic,
+        [MarshalAs(UnmanagedType.Bool)] out bool isUnderlined,
+        [MarshalAs(UnmanagedType.Bool)] out bool isMonospace,
+        [MarshalAs(UnmanagedType.Bool)] out bool isSerif,
+        [MarshalAs(UnmanagedType.Bool)] out bool isSmallCaps,
+        out int pointSize,
+        out int fontId);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorWordIsFromDictionary")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool TessResultIteratorWordIsFromDictionary(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorWordIsNumeric")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool TessResultIteratorWordIsNumeric(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorSymbolIsSuperscript")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool TessResultIteratorSymbolIsSuperscript(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorSymbolIsSubscript")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool TessResultIteratorSymbolIsSubscript(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorSymbolIsDropcap")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool TessResultIteratorSymbolIsDropcap(IntPtr iterator);
+
+    [LibraryImport(DllName, EntryPoint = "TessResultIteratorGetWordStrAttributes")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial IntPtr TessResultIteratorGetWordStrAttributes(IntPtr iterator,
+        out int isBold, out int isItalic, out int isUnderlined, out int isMonospace, out int isSerif,
+        out int isSmallCaps, out int pointSize, out int fontId);
 }
